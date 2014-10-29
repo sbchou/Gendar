08395afda04578938742c5c0dfb8d522ad410b8b
cat <sbchou@gmail.com>
2014-10-29 16:52:11 -0400

    nicely formatte output

--- ultimatesexmachine.py
+++ ultimatesexmachine.py
@@ -6,47 +6,59 @@ from hammock import Hammock as GendreAPI
 import string
 
 """ 
-Test various gender libraries on training, testing!
+Test various gender libraries on training, data!
 """
-d = gender.Detector()
-gendre = GendreAPI("http://api.namsor.com/onomastics/api/json/gendre")
-
+# Initialize all yo shit
 CONSUMER_KEY = "RE9RJs5c3zQ8yhLCZQCKlVglT"
 CONSUMER_SECRET = "iA5ElxRl9JWm4wYDntSI2UxT56Yp6SpcDkAJ40EoDvMMFnWkfK"
 ACCESS_TOKEN = "359150678-DAyroyYOpYkqLiCaNIok2M9KoY2fj1C4fxBO5v6R"
 ACCESS_TOKEN_SECRET= "zhTmAekrR0hcX4yfV4mRLVx1OQZMuIikj3dTvtfzghjAZ"
 
 api = twitter.Api(consumer_key = CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token_key=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+sexmachine = gender.Detector()
+gendre = GendreAPI("http://api.namsor.com/onomastics/api/json/gendre")
 
-testing = json.load(open('data/labels.json', 'r'))
-ids = testing.keys()
 
-# We that are rate-limited
-n = 200
-twps = api.UsersLookup(user_id=ids[:100])
+def infer_sex(data_path, output_path, n=100):
+    data = json.load(open(data_path, 'r'))
+    output = open(output_path, 'w')
 
-for t in twps:
-    screen_name = t.screen_name
-    name = t.name
+    ids = data.keys()
+    twps = api.UsersLookup(user_id=ids[:n])
 
-    # WE SPEAK AMERICAN HERE
-    cleanedname = filter(lambda x: x in string.printable, name).strip()   
-    if cleanedname:
-        splitname = cleanedname.split()
-        firstname = splitname[0]
-        lastname = splitname[-1] if len(splitname) > 1 else None
+    output.write("name\tlocation\tgender_gold\tgender_sexmachine\tgender_genderize\tgender_gendre\n")
 
-        location = t.location
-        gender_gold = testing[str(t.id)]
-        gender_sexmachine = d.get_gender(firstname)
-        gender_genderize = Genderize().get([firstname])[0]['gender']
-       
-        #import pdb; pdb.set_trace() 
+    for t in twps:
+        screen_name = t.screen_name
+        name = t.name
+        location = t.location if t.location else "None"
 
-        resp = gendre(firstname, lastname).GET() # also add country code?
-        gender_gendre = resp.json().get('gender')
+        # WE SPEAK AMERICAN HERE
+        cleanedname = filter(lambda x: x in string.printable, name).strip()   
+        if cleanedname:
+            splitname = cleanedname.split()
+            firstname = splitname[0]
+            lastname = splitname[-1] if len(splitname) > 1 else None
+            gender_gold = data[str(t.id)]
+            gender_sexmachine = sexmachine.get_gender(firstname)
+            gender_genderize = Genderize().get([firstname])[0]['gender']
+           
+            #import pdb; pdb.set_trace() 
 
-        print name, location, gender_gold, gender_sexmachine, gender_genderize, gender_gendre
+            resp = gendre(firstname, lastname).GET() # also add country code?
+            gender_gendre = resp.json().get('gender')
+            
+            values = [name, location, gender_gold, gender_sexmachine, \
+                    gender_genderize, gender_genderize]
+    
+            values = [v.encode('ascii', 'ignore') if v is not None else "None" for v in values]
+
+            #print gender_sexmachine, type(gender_sexmachine), gender_genderize, type(gender_genderize), gender_gendre, type(gender_gendre)
+            output.write("\t".join(values) + "\n")
+
+    output.close()
+
+infer_sex('data/labels.json', 'data/output.tsv', 100)
 
 
 
